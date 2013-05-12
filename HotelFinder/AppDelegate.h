9d053fa1199fa023e952c2c9596cda4f3e959752
Dawn <wangsguang@gmail.com>
2013-05-12 15:32:32 +0800

    HotelFinder文件

---
+++ HotelFinder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HotelFinder
+//
+//  Created by Dawn on 13-5-12.
+//  Copyright (c) 2013年 Dawn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HotelFinder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HotelFinder
+//
+//  Created by Dawn on 13-5-12.
+//  Copyright (c) 2013年 Dawn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
